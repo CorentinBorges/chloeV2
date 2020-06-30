de7672f0d62b93cc1a16b22a1952737fabbfe3b9
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-06-30 11:48:11 +0200

    zoom on pictures

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,7 @@
 import '../css/app.scss';
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
+require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -2,6 +2,4 @@
 
 return array (
   'DATABASE_URL' => NULL,
-  'DB_PASS' => NULL,
-  'DB_USER' => NULL,
 );
