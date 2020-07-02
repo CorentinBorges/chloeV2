52ee8ea2d72b2c29406487a1327efaa9b99cba8f
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-07-02 10:40:16 +0200

    user fixture created

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
-
+/*TODO: not gitignor+change values*/
     /**
      * @var UserPasswordEncoderInterface
      */
